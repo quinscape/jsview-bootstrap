41d4c516ed61b6f51b755f8628a76a4e89a725cd
Sven Helmberger <fforw@gmx.de>
2018-06-12 21:09:20 +0200

    Move bootstrap into its own package

---
+++ index.js
@@ -0,0 +1,31 @@
+var React = require("react")
+var render = require("react-dom").render;
+var domready = require("domready")
+
+/**
+ * Bootstrap function that mounts a react element on a node #root
+ *
+ * @param {function} renderFn render function
+ * @param {function} cb     callback to call after rendering
+ */
+module.exports = function (renderFn, cb) {
+    domready(
+        function () {
+                const elem = document.getElementById("root-data");
+                const data = !!elem && JSON.parse(elem.innerHTML);
+
+                const element = renderFn(data);
+
+                if (element !== false && !React.isValidElement(element))
+                {
+                    throw new Error("Render function returned no React Element, but "  + element);
+                }
+
+                render(
+                    element,
+                    document.getElementById("root"),
+                    cb
+                );
+        }
+    );
+}
